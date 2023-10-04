94a51c32f35a6c9c4af730139c9351c37aa14bed
Gürgün Dayıoğlu <gurgun.dayioglu@icloud.com>
2023-10-04 20:13:15 +0200

    remove web support

--- html.js
+++ html.js
@@ -1,62 +1,56 @@
-(() => {
-  "use strict";
+"use strict";
 
-  const escapeDict = {
-    '"': "&quot;",
-    "'": "&apos;",
-    "&": "&amp;",
-    "<": "&lt;",
-    ">": "&gt;",
-  };
+const escapeDict = {
+  '"': "&quot;",
+  "'": "&apos;",
+  "&": "&amp;",
+  "<": "&lt;",
+  ">": "&gt;",
+};
 
-  const escapeRE = new RegExp(`[${Object.keys(escapeDict).join("")}]`, "gv");
+const escapeRegExp = new RegExp(`[${Object.keys(escapeDict).join("")}]`, "gv");
 
-  const escapeReplacer = (key) => escapeDict[key];
+const escapeReplacer = (key) => escapeDict[key];
 
-  const stringify = (exp) =>
-    typeof exp === "string"
-      ? exp
-      : Array.isArray(exp)
-      ? exp.join("")
-      : exp?.toString() ?? "";
+const stringify = (exp) =>
+  typeof exp === "string"
+    ? exp
+    : Array.isArray(exp)
+    ? exp.join("")
+    : exp?.toString() ?? "";
 
-  /**
-   * @param {{ raw: string[] }} literals
-   * @param {...*} expressions
-   * @returns {string}
-   */
-  const html = ({ raw: literals }, ...expressions) => {
-    switch (literals.length) {
-      case 0:
-        return "";
-      case 1:
-        return literals[0];
-    }
-
-    const lastLitIndex = literals.length - 1;
-    let acc = "";
-
-    for (let i = 0; i < lastLitIndex; ++i) {
-      let lit = literals[i];
-      let str = stringify(expressions[i]);
-
-      if (lit && lit[lit.length - 1] === "!") {
-        lit = lit.slice(0, -1);
-      } else if (str) {
-        str = str.replace(escapeRE, escapeReplacer);
-      }
-
-      acc += lit += str;
-    }
-
-    acc += literals[lastLitIndex];
-
-    return acc;
-  };
-
-  if (typeof process === "object" && process?.versions?.node) {
-    module.exports.html = html;
-  } else {
-    globalThis.html = html;
+/**
+ * @param {{ raw: string[] }} literals
+ * @param {...*} expressions
+ * @returns {string}
+ */
+const html = ({ raw: literals }, ...expressions) => {
+  switch (literals.length) {
+    case 0:
+      return "";
+    case 1:
+      return literals[0];
   }
-})();
+
+  const lastLitIndex = literals.length - 1;
+  let acc = "";
+
+  for (let i = 0; i < lastLitIndex; ++i) {
+    let lit = literals[i];
+    let str = stringify(expressions[i]);
+
+    if (lit && lit[lit.length - 1] === "!") {
+      lit = lit.slice(0, -1);
+    } else if (str) {
+      str = str.replace(escapeRE, escapeReplacer);
+    }
+
+    acc += lit += str;
+  }
+
+  acc += literals[lastLitIndex];
+
+  return acc;
+};
+
+module.exports.html = html;
